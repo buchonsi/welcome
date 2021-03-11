0a62aae107d53ae0c542b35609cddcfd6f136f67
buchonsi <73854927+buchonsi@users.noreply.github.com>
2021-03-11 19:05:38 +0900

    version2

--- Prob5.java
+++ Prob5.java
@@ -1,4 +1,4 @@
-package com.kosta.day17;
+package com.kosta.day17.reviewexam;
 
 public class Prob5 {
 	public static void main(String[] args) {
@@ -6,8 +6,16 @@ public class Prob5 {
 		int[] counter = new int[4];
 
 		// 구현하시오 - 숫자들의 개수를 세어 저장하는 코드를 작성한다.
+		for(int num:answer) {			// answer배열에 있는 숫자를 보고 카운트를 세는 방법
+			counter[num-1]++;
+		}
+		
 		
 		// 구현하시오 - 출력결과와 같이 나오도록 작성한다.
+		for(int i=0; i<counter.length;i++) {
+			System.out.println((i+1)+"의 갯수는 "+counter[i]+"개 입니다.");
+		}
+		
 		
 
 	}
